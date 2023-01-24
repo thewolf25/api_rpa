08c2f8fb0fb933552e59cd511e1d043b72a72d46
Mohamed Mehdi Nasri <nasrimehdi96@gmail.com>
2023-01-24 09:34:05 +0100

    upload project

---
+++ src/main/java/com/rpa/api_rpa/ApiRpaApplication.java
@@ -0,0 +1,13 @@
+package com.rpa.api_rpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpa/api_rpa/ApiRpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpa.api_rpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
